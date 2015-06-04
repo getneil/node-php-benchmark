4e6ec1a54a6b213b699d0ef64a49f1f3cda6766b
Josephk <josephk@meditab.com>
2015-06-04 19:22:37 +0800

    added js app files

---
+++ js/index.js
@@ -0,0 +1,45 @@
+var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'benchmark'
+});
+var app = express();
+
+connection.connect(function(err){
+  if(!err) {
+      console.log("Database is connected ... \n\n");  
+  } else {
+      console.log("Error connecting database ... \n\n");  
+  }
+});
+
+app.get("/",function(req,res){
+  console.log('Benchmarker!');
+});
+
+app.get("/list",function(req,res){
+  connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
+    // connection.end();
+      if (!err)
+        console.log('The solution is: ', rows);
+      else
+        console.log('Error while performing Query.');
+      });
+});
+
+app.post("/create",function(req,res){
+  var post = req.body;
+  console.log(post,'post');
+  connection.query('INSERT INTO user SET ?',post, function(err, rows, fields) {
+    // connection.end();
+      if (!err)
+        console.log('The solution is: ', rows);
+      else
+        console.log(err,'Error while performing Query.');
+      });
+});
+
+app.listen(3003);
