6489140e26e125c7b3fa5ba82b4f5ecc9961b0ee
Josephk <josephk@meditab.com>
2015-06-11 19:36:23 +0800

    refactored

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,17 +1,20 @@
 var loadtest = require('loadtest');
 var body     = require('./load');
+var ip = "192.168.9.2";
+var port  = 3003;
+var method = "GET";
 
 var options  = {
-    // url: 'http://localhost:3003/create',
-    url: 'http://localhost:3003/list',
-    // concurrency: 1000,
+    // url: 'http://'+ip+':'+port+'/create',
+    url: 'http://'+ip+':'+port+'/list',
+    concurrency: 2000,
     // maxRequests: 1000,
     maxSeconds: 60,
-    method: 'GET',
+    method: method,
     // method: 'POST',
     // body: body,
     contentType: 'application/json',
-    requestsPerSecond: 300,
+    // requestsPerSecond: 300,
 };
 loadtest.loadTest(options, function (error, result)
 {
--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var app = express();
 
 var pool = mysql.createPool({
   connectionLimit : 100,
-  acquireTimeout: 30000,
+  acquireTimeout: 999999,
   // host      : process.env.MYSQL_PORT_3306_TCP_ADDR || '192.168.30.11',
   // port      : process.env.MYSQL_PORT_3306_TCP_PORT || 4004,
   host      : '192.168.9.2',
@@ -22,8 +22,99 @@ console.log("Benchmarker!");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-function handleDatabase(req, res){
-  var post = undefined;
+// function handleDatabase(req, res){
+//   var post = undefined;
+//   pool.getConnection(function(err, connection){
+//     if(err){
+//       console.log(err,'err connection')
+//       connection.release();
+//       res.json({"code":100, "status":"Error in database connection"});
+//       return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+
+//     // async hashing
+//     if(req.method == 'POST'){
+//       post = req.body;
+//       if(post.password){
+//         bcrypt.hash(post.password, 12, function(err, hash) {
+//           post.password = hash
+//           console.log(post,'post')
+//           query = connection.query('INSERT INTO user SET ?', post, function(err,rows){
+//             connection.release();
+//             if(err){
+//               console.log(err,'err')
+//             }else{
+//               res.json(rows);
+//             }
+//           });
+//           // console.log(query.sql,'query')
+//         });
+//       }
+//     }else if(req.method == 'GET'){
+//       console.log('list')
+//       connection.query('SELECT * FROM user LIMIT 1000', function(err,rows){
+//         connection.release();
+//         if(err){
+//           console.log(err,'err')
+//         }else{
+//           res.json(rows);
+//         }
+//       });
+//     }
+
+//     connection.on("error", function(err){
+//       res.json({"code":100, "status": "Error in database connection"});
+//       return;
+//     });
+//   });
+// }
+
+// app.get("/list",function(req,res){
+//   handleDatabase(req,res);
+// });
+
+// app.post("/create",function(req,res){
+//   handleDatabase(req,res);
+// });
+
+
+// app.listen(3003);
+
+
+
+function postUser(req, res){
+  var post = req.body;
+  if(post.password){
+    bcrypt.hash(post.password, 12, function(err, hash) {
+      post.password = hash
+      pool.getConnection(function(err, connection){
+        if(err){
+          console.log(err,'err connection')
+          connection.release();
+          res.json({"code":100, "status":"Error in database connection"});
+          return;
+        }
+        console.log("connected as id " + connection.threadId);
+        query = connection.query('INSERT INTO user SET ?', post, function(err,rows){
+          connection.release();
+          if(err){
+            console.log(err,'err')
+          }else{
+            res.json(rows);
+          }
+        });
+        // console.log(query.sql,'query')
+        connection.on("error", function(err){
+          res.json({"code":100, "status": "Error in database connection"});
+          return;
+        });
+      }) 
+    });
+  }
+}
+
+function listUser(req, res){
   pool.getConnection(function(err, connection){
     if(err){
       console.log(err,'err connection')
@@ -32,61 +123,28 @@ function handleDatabase(req, res){
       return;
     }
     console.log("connected as id " + connection.threadId);
-    
-    switch(req.url){
-      case '/list':
-        queryStr = 'SELECT * FROM user LIMIT 1000';
-        break;
-      case '/create':
-        queryStr = 'INSERT INTO user SET ?';
-        break;
-      default:
-        queryStr = 'SELECT * FROM user LIMIT 1';
-    }
-
-    // async hashing
-    if(req.method == 'POST'){
-      post = req.body;
-      if(post.password){
-        bcrypt.hash(post.password, 12, function(err, hash) {
-          post.password = hash
-          console.log(post,'post')
-          query = connection.query(queryStr, post, function(err,rows){
-            connection.release();
-            if(err){
-              console.log(err,'err')
-            }else{
-              res.json(rows);
-            }
-          });
-          // console.log(query.sql,'query')
-        });
+    connection.query('SELECT * FROM user LIMIT 1000', function(err,rows){
+      connection.release();
+      if(err){
+        console.log(err,'err')
+      }else{
+        res.json(rows);
       }
-    }else if(req.method == 'GET'){
-      console.log('list')
-      connection.query(queryStr, function(err,rows){
-        connection.release();
-        if(err){
-          console.log(err,'err')
-        }else{
-          res.json(rows);
-        }
-      });
-    }
-
+    });
     connection.on("error", function(err){
       res.json({"code":100, "status": "Error in database connection"});
       return;
     });
-  });
+  })
 }
 
+
 app.get("/list",function(req,res){
-  handleDatabase(req,res);
+  listUser(req,res);
 });
 
 app.post("/create",function(req,res){
-  handleDatabase(req,res);
+  postUser(req,res);
 });
 
 
