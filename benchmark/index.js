cfa1f4deee2c5aaf4d436984a7039f2525348ca1
Christopher Rey Pantoja <christopherp@meditab.com>
2015-06-17 16:53:29 +0800

    added arguments to running benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,27 +1,50 @@
-var loadtest = require('loadtest');
-var body     = require('./load');
-var ip = "192.168.9.5";
-var port  = 4000;
-var method = "POST";
-var options  = {
-    // url: 'http://'+ip+':'+port+'/create',
-    url: 'http://'+ip+':'+port+'/',
-    concurrency: 1,
-    maxRequests: 1,
-    maxSeconds: 1,
-    method: method,
-    body: body,
-    // contentType: 'application/x-www-form-urlencoded'
-    contentType: 'application/json',
-    // requestsPerSecond: 300,
+/*
+node index.js create/list js/php concurrency
+create -> POST
+list -> GET
+
+js -> 3003
+php -> 4000
+ */
+var action      = "";
+var language    = "";
+var concurrency = 0;
+var port        = 0;
+var method      = "";
+var ip          = "localhost";
+
+process.argv.forEach(function (val, index) {
+    switch (index) {
+        case 2:
+            action = val.toLowerCase();
+            method = (val == 'CREATE') ? 'POST' : 'GET';
+            break;
+        case 3:
+            language = val;
+            port     = (val == 'JS') ? 3003 : 4000;
+            break;
+        case 4:
+            concurrency = +val;
+            break;
+    }
+});
+var loadtest    = require('loadtest');
+var body        = require('./load');
+var options     = {
+    url         : 'http://'+ip+':'+port+'/'+action,
+    concurrency : concurrency,
+    maxRequests : 1,
+    maxSeconds  : 1,
+    method      : method,
+    body        : body,
+    contentType : 'application/json',
 };
 loadtest.loadTest(options, function (error, result)
 {
-    console.log(options,'opt')
     if (error)
     {
         return console.error('Got an error: %s', error);
     }
-    console.log(error, result);
+    console.log(result);
     console.log('Tests run successfully');
 });
